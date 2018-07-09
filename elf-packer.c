424f7cb019e70b17f41ab7fb674d419804376b09
atto <ak.tamalin@gmail.com>
2018-07-09 22:59:32 +0900

    ugokanai

--- elf-packer.c
+++ elf-packer.c
@@ -10,6 +10,7 @@
 
 #define DUMP(x) do {printf("  " #x " = %u(0x%x)\n", (uint32_t)x, (uint32_t)x);} while(0);
 
+#define DWORD uint32_t
 
 void dump_ehdr(Elf32_Ehdr *ehdr){
 	int i;
@@ -50,6 +51,61 @@ void dump_shdr(Elf32_Shdr *shdr, int e_shnum){
         printf("\n");
     }
     printf("\n");
+}
+
+void xor_encoder(unsigned char *start, unsigned int size, unsigned char encoder)
+{
+	unsigned int	cnt=0;
+
+	printf("Start Xor Encode by '0x%X'\n", encoder);
+	for(cnt=0; cnt<size; cnt++){
+		start[cnt] ^= encoder;
+	}
+	printf("Encode Done\n");
+}
+
+unsigned char decode_stub[] = {
+	0x60,				// pushad
+	0xBE,0xFF,0xFF,0xFF,0xFF,	// mov esi, decode_start
+	0xB9,0xFF,0xFF,0xFF,0xFF,	// mov ecx, decode_size
+//	0x81,0xC6,0xFF,0xFF,0xFF,0xFF,	// add esi, base_addr 
+	0xB0,0xFF,			// mov al, decoder
+	0x30,0x06,			// xor byte ptr [esi], al (LOOP)
+	0x46,				// inc esi
+	0x49,				// dec ecx
+	0x75,0xFA,			// jnz LOOP
+	0x61,				// popad
+	0xE9,0xFF,0xFF,0xFF,0xFF	// jmp OEP
+};
+
+unsigned int decode_start_offset = 2;
+unsigned int decode_size_offset  = 7;
+unsigned int base_address_offset = 13 - 6;
+unsigned int decoder_offset      = 18 - 6;
+unsigned int jmp_oep_addr_offset = 27 - 6;
+
+void create_decode_stub(unsigned int code_vaddr, unsigned int code_vsize,
+		unsigned char decoder, unsigned int oep)
+{
+	int	cnt=0;
+	int	jmp_len_to_oep=0;
+
+	jmp_len_to_oep = oep - (code_vaddr + code_vsize + sizeof(decode_stub));
+
+	printf("start   : 0x%08X\n", code_vaddr);
+	printf("size    : 0x%08X\n", code_vsize);
+	printf("decoder : 0x%02X\n", decoder);
+	printf("oep     : 0x%08X\n", oep);
+	printf("jmp len : 0x%08X\n", jmp_len_to_oep);
+
+	memcpy(&decode_stub[decode_start_offset], &code_vaddr, sizeof(DWORD));
+	memcpy(&decode_stub[decode_size_offset],  &code_vsize, sizeof(DWORD));
+	//memcpy(&decode_stub[base_address_offset],  &base_addr, sizeof(DWORD));
+	memcpy(&decode_stub[decoder_offset],  &decoder, sizeof(unsigned char));
+	memcpy(&decode_stub[jmp_oep_addr_offset],  &jmp_len_to_oep, sizeof(DWORD));
+
+	return;
+
 }
 
 Elf32_Shdr *search_oep_include_section_header(
@@ -116,7 +172,7 @@ int main(int argc, char *argv[]){
 
 	ehdr = (Elf32_Ehdr *)target_bin_buffer;
 	shdr = (Elf32_Shdr *)(&target_bin_buffer[ehdr->e_shoff]);
-	
+
 	/*
 	//dump header
 	dump_ehdr(ehdr);
@@ -138,7 +194,7 @@ int main(int argc, char *argv[]){
 	xor_encoder((unsigned char *)(oep_shdr->sh_offset + target_bin_buffer), oep_shdr->sh_size, encoder);
 
 	//create xor decode
-	create_decode_stub(section_vaddr, section_vsize, /*base_addr*/, encoder, oep);
+	create_decode_stub(section_vaddr, section_vsize, encoder, ehdr->e_entry);
 	memcpy((unsigned char *)(section_raddr + section_vsize + target_bin_buffer), decode_stub, sizeof(decode_stub));
 
 	oep_shdr->sh_size = section_rsize;
